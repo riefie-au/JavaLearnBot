93e30ef36a852926bbce55319abd6e4ad222aa68
riefie-au <bhebhefieldad@gmail.com>
2025-12-11 20:56:44 +0800

    changed navigation bar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module lecture.javalearnbot {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens lecture.javalearnbot to javafx.fxml;
     exports lecture.javalearnbot;
