b45d044f64a0836774ad3580ccef74583c5e7097
rie <156742040+riefie-au@users.noreply.github.com>
2025-12-13 19:06:48 +0800

    Made layout responsive n made navigation efficient

--- src/main/java/lecture/javalearnbot/JavaLearnBot.java
+++ src/main/java/lecture/javalearnbot/JavaLearnBot.java
@@ -3,6 +3,7 @@ package lecture.javalearnbot;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,11 @@ public class JavaLearnBot extends Application {
     public void start(Stage stage) throws IOException {
 
         //ChatPageUI
-        FXMLLoader chatPage = new FXMLLoader(JavaLearnBot.class.getResource("homePage.fxml"));
-        Scene scene = new Scene(chatPage.load());
+        FXMLLoader loader = new FXMLLoader(JavaLearnBot.class.getResource("navigationBar.fxml"));
+        BorderPane root = loader.load();
+        NavigationController nav = loader.getController();
+        nav.toHome();
+        Scene scene = new Scene(root);
         stage.setTitle("Chat Page");
         stage.setScene(scene);
         stage.show();
